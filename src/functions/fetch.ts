21b6806f676b44afe0835453d992aa9b2a7465eb
teitei-tk <teitei.tk@gmail.com>
2019-03-15 01:16:14 +0900

    search latest post

--- src/functions/fetch.ts
+++ src/functions/fetch.ts
@@ -1,7 +1,25 @@
+import * as Puppeteer from "puppeteer";
+
 import { Request, Response } from "express";
 
-export const fetchFunction = (_: Request, res: Response) => {
+export const fetchFunction = async (_: Request, res: Response) => {
+  const url = "https://dabun.net/";
+
+  const browser = await Puppeteer.launch({
+    args: ["--no-sandbox"]
+  });
+
+  const page = await browser.newPage();
+  await page.goto(url);
+
+  const latest = await page.$eval(".permanentlink > a:last-child", el => {
+    return el.getAttribute("href").replace("/news/", "");
+  });
+
+  browser.close();
+
   return res.status(200).json({
-    message: "success"
+    message: "success",
+    url: latest
   });
 };
